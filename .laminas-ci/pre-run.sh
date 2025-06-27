014922ade7087c096ea23ca70f70f764e1bd7bea
sergiu <sergiubota@rospace.com>
2025-06-27 14:23:29 +0300

    install ext-redis for CI checks
    Signed-off-by: sergiu <sergiubota@rospace.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-# Due to the fact that we are disabling plugins when installing/updating/downgrading composer dependencies
-# we have to manually enable the coding standard here.
-composer enable-codestandard
+JOB=$3
+
+COMMAND=$(echo "${JOB}" | jq -r '.command // ""')
+[[ "${COMMAND}" =~ ^REDIS_VERSION=([0-9\.]+) ]] || exit 0
+
+PHP=$(echo "${JOB}" | jq -r '.php // ""')
+REDIS_VERSION=${BASH_REMATCH[1]}
+
+echo "SETUP: Installing ext-redis $REDIS_VERSION with PHP $PHP..."
+pecl install -f --configureoptions 'enable-redis-igbinary="yes" enable-redis-lzf="yes"' igbinary redis-${REDIS_VERSION}
+
+if [ $? -ne 0 ]; then
+  echo "ERROR: Installation of ext-redis $REDIS_VERSION with PHP $PHP failed."
+  exit 1
+fi
+
+echo "extension=redis.so" > /etc/php/${PHP}/mods-available/redis.ini
