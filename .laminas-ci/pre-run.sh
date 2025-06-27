dc7d40f9eb7755cf7a4585b277772fddae7e6b6f
sergiu <sergiubota@rospace.com>
2025-06-27 14:42:24 +0300

    add redis extension
    Signed-off-by: sergiu <sergiubota@rospace.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 PHP_VERSION="$1"
-apt install make
-pecl install swoole
-echo "extension=swoole.so" > /etc/php/${PHP_VERSION}/mods-available/60-swoole.ini
-phpenmod -v ${PHP} -s cli swoole
+
+apt update
+apt install -y make php-dev php-pear
+
+pecl install redis
+
+echo "extension=redis.so" | sudo tee /etc/php/${PHP_VERSION}/mods-available/redis.ini
+
+sudo phpenmod -v ${PHP_VERSION} -s cli redis
