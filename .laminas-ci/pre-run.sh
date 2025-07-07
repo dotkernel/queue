2603c1d13a1b4b76330ae1affebfd298d76f6076
sergiu <sergiubota@rospace.com>
2025-07-07 12:16:18 +0300

    install redis extension
    Signed-off-by: sergiu <sergiubota@rospace.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -2,6 +2,6 @@ JOB=$3
 PHP_VERSION=$4
 COMMAND=$(echo "${JOB}" | jq -r '.command')
 
-echo "Running pre-run $COMMAND"
+echo "Running pre-run  $COMMAND"
 
-apt-get install php-redis
+apt-get install php"${PHP_VERSION}"--php-redis
