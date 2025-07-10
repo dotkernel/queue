320ed39a16b6aec6a6eb11d949c45509e1adcae1
Alex Karajos <alex.merlin.1985@gmail.com>
2025-07-10 10:01:25 +0300

    Update pre-run.sh

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt install php-redis
+phpenmod redis
+
 # Due to the fact that we are disabling plugins when installing/updating/downgrading composer dependencies
 # we have to manually enable the coding standard here.
 composer enable-codestandard
