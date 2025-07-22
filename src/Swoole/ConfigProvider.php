a8870e489b0fce31dbe0da1e65cb314ce474b8a0
bota <Bota@dotkernel.com>
2025-07-22 10:17:17 +0300

    manage logs for processed and failed messages
    Signed-off-by: bota <Bota@dotkernel.com>

--- src/Swoole/ConfigProvider.php
+++ src/Swoole/ConfigProvider.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Queue\Swoole;
 
+use Dot\DependencyInjection\Factory\AttributedServiceFactory;
 use Queue\Swoole\Command\Factory\StartCommandFactory;
 use Queue\Swoole\Command\Factory\StopCommandFactory;
+use Queue\Swoole\Command\GetFailedMessagesCommand;
+use Queue\Swoole\Command\GetProcessedMessagesCommand;
 use Queue\Swoole\Command\StartCommand;
 use Queue\Swoole\Command\StopCommand;
 use Queue\Swoole\Delegators\TCPServerDelegator;
@@ -27,10 +30,12 @@ class ConfigProvider
                 TCPSwooleServer::class => [TCPServerDelegator::class],
             ],
             "factories"  => [
-                TCPSwooleServer::class => ServerFactory::class,
-                PidManager::class      => PidManagerFactory::class,
-                StartCommand::class    => StartCommandFactory::class,
-                StopCommand::class     => StopCommandFactory::class,
+                TCPSwooleServer::class             => ServerFactory::class,
+                PidManager::class                  => PidManagerFactory::class,
+                StartCommand::class                => StartCommandFactory::class,
+                StopCommand::class                 => StopCommandFactory::class,
+                GetProcessedMessagesCommand::class => AttributedServiceFactory::class,
+                GetFailedMessagesCommand::class    => AttributedServiceFactory::class,
             ],
             "aliases"    => [],
         ];
