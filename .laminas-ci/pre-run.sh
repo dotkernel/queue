34b4812bb30c276964e35c7ac96741ef128cb9d8
arhimede <julian@dotkernel.com>
2025-07-30 22:48:05 +0300

    redis missing
    Signed-off-by: arhimede <julian@dotkernel.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -2,4 +2,4 @@
 
 # Due to the fact that we are disabling plugins when installing/updating/downgrading composer dependencies
 # we have to manually enable the coding standard here.
-composer enable-codestandard
+#composer enable-codestandard
