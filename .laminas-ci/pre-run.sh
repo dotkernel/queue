1497db43b8c16363cf47c445ba0520a0b2306538
bota <Bota@dotkernel.com>
2025-08-01 14:01:41 +0300

    reverted pre-run
    Signed-off-by: bota <Bota@dotkernel.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,8 +1,5 @@
-apt-get update
-apt-get install -y php-dev php-pear libcurl4-openssl-dev libssl-dev gcc make autoconf
+JOB=$3
+PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
 
-pecl channel-update pecl.php.net
-
-pecl install -f swoole
-
-echo "extension=swoole.so" > /etc/php/cli/conf.d/20-swoole.ini
+apt update
+apt install -y "php${PHP_VERSION}-swoole"
